3ea738b89d7b8d3071f77d61d38019dfce47af5f
michellinet <michellinet@procore.com>
2017-07-14 09:21:53 -0700

    Initial Commit

---
+++ Dijkstra/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dijkstra
+//
+//  Created by Michelline Tran on 7/14/17.
+//  Copyright © 2017 Michelline Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
